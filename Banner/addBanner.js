ff6ead7d1aec412ba4d09c2ef1396b200ee1767a
Abubakkar <abubakkarsajid4@gmail.com>
2025-06-22 09:29:46 +0500

    Works on server

--- Banner/addBanner.js
+++ Banner/addBanner.js
@@ -1,4 +1,5 @@
 const Banner = require("../schema/banner-schema");
+const cloudinary = require("../utils/cloudinary"); // Cloudinary config file
 
 const addBanner = async (req, res) => {
   const { image, url, status, expiry } = req.body;
@@ -8,14 +9,21 @@ const addBanner = async (req, res) => {
   }
 
   try {
+    // Upload base64 image to Cloudinary
+    const uploadResult = await cloudinary.uploader.upload(image, {
+      folder: "banners",
+    });
+
+    // Save only the Cloudinary URL in MongoDB
     const newBanner = new Banner({
-      image, // base64 image string
+      image: uploadResult.secure_url,
       url,
       status: status || "active",
       expiry,
     });
 
     await newBanner.save();
+
     res.status(201).json({ message: "Banner added successfully.", banner: newBanner });
   } catch (error) {
     console.error("Error adding banner:", error);
---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dha65z0gy",
+  api_key: "483687736517735",
+  api_secret: "DSFo5Jd3Wtc4j8mc3PVqTtCq-pY",
+});
+
+module.exports = cloudinary;
