7675dfd47d8901f196f2582870c4dac45359436d
Abubakkar <abubakkarsajid4@gmail.com>
2025-06-24 15:44:08 +0500

    Work on massege

--- routers/Routes.js
+++ routers/Routes.js
@@ -38,89 +38,78 @@ const getAllMerchants = require("../Merchant/getAllMerchants");
 const getMerchantById = require("../Merchant/getMerchantById");
 const getBannerbyid = require("../Banner/getBannerById");
 const getFeedbackById = require("../Feedback/getFeedbackById");
-const chatRouter = require("../chats/ChatRouter")
+const chatRouter = require("../chats/ChatRouter");
 
-// account routes
+// Account routes
 router.post("/account-creation/:id", signup);
 router.post("/login", login);
-router.post("/user/account/delete/a/time", deleteUser);
-
-// get user
-router.get("/a/admin/bsvcr/get/all/users", getAlluser);
-
+router.post("/user/account/delete", deleteUser);
 router.post("/update-user", updateUser);
 router.get("/client/get/user/:ui_id", getUserUIID);
-// passwordReset
-router.post("/client/reset-password", passwordResetOtp);
-router.post(
-  "/client/reset-password/new-password",
-  require("../account/newPassword")
-);
 
-// feedback routes
+// Password reset
+router.post("/client/reset-password", passwordResetOtp);
+router.post("/client/reset-password/new-password", require("../account/newPassword"));
+
+// Feedback
 router.post("/client/feedback", postFeedback);
 router.get("/admin/all/feedback", getFeedback);
 router.post("/admin/update/feedback", updateStatus);
-router.get("/admin/get/feedback/:id",getFeedbackById);
+router.get("/admin/get/feedback/:id", getFeedbackById);
 
-// buy coins
+// Users
+router.get("/admin/get/all/users", getAlluser);
+
+// Coin transactions
 router.post("/client/buy-coins", buyCoins);
 router.post("/coin-buy/merchant/local", coinbuymerchant);
 router.post("/merchant-approve/coin/user", approvebuycoin);
 router.post("/admin/approve/coin/buy/users", approveCoinBuyLocalUsers);
-router.get("/get/all/transaction/admin/a/", getAllTransaction);
-router.get("/client/admin/get/transaction/a/i/d/:ui_id", getTransactionByUIID);
+router.get("/admin/get/all/transactions", getAllTransaction);
+router.get("/admin/get/transactions/:ui_id", getTransactionByUIID);
 
-//merchant
+// Merchants
 router.post("/apply-merchant", applyMerchant);
 router.post("/admin/approve/merchant", approveMerchant);
 router.post("/merchant/buy-coin", require("../Merchant/merchantCoinBuy"));
 router.post("/admin/approve/coin", require("../Merchant/adminapprove-coin"));
-router.post("/merchant/approve/sell/coin/a/live", approveCoinsell);
-router.get("/get/all/a/vvpi/merchants", getAllMerchants);
-router.get("/get/merchant/user/o/bsvcr/user/find/:ui_id",getMerchantById)
+router.post("/merchant/approve/sell-coin", approveCoinsell);
+router.get("/admin/get/all/merchants", getAllMerchants);
+router.get("/admin/get/merchant/:ui_id", getMerchantById);
 
-// withdrawal
+// Withdrawals
 router.post("/client/withdrawal/request", requestWithdrawal);
 router.post("/admin/approve/withdrawal", require("../admin/approve-withdrawl"));
 router.get("/admin/get/all/withdraws", getAllWithdraws);
-router.get("/get/user/withdraws/info/:ui_id", getwithdrawByUIID);
+router.get("/client/get/withdraws/:ui_id", getwithdrawByUIID);
 
-// coin price
+// Coin price
 router.post("/admin/update/coin-price", require("../coins/coinPrice"));
 router.get("/get/coin-price", getCoinPrice);
 
-// gift send
+// Gifts
 router.post("/client/gift/send", sendGift);
 
-// baner add
+// Banners
 router.post("/admin/add/banner", addBanner);
 router.get("/client/get/banner", getBanner);
-router.get("/client/get/banner/i/d/full/n/:id",getBannerbyid)
+router.get("/client/get/banner/:id", getBannerbyid);
 
-// setInterval(() => {
-//   autoExpireBanners();
-// }, 60 * 1000); // every 1 minute
-
-// user post thing
+// Posts
 router.post("/client/post/create", createPost);
 router.get("/client/post/get", getAllPost);
 router.post("/client/post/get/user", getPostUser);
 router.post("/client/post/like", likePostUser);
 router.post("/client/post/comment", commentPostUser);
 
-// follow/following
+// Follow
 router.post("/client/follow", followUser);
 
-// store device token
+// Device token
 router.post("/client/store-device-token", storeDeviceToken);
 
-// chats by admin 
+// Chats
 router.use("/chats", chatRouter);
-
-
-// user chat routes
 router.use("/chats/users", chatRouter);
-
 
 module.exports = router;
