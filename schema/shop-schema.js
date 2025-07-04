0836701c8bfe7be0bc70bfd998e43e07ba9478b1
Abubakkar <abubakkarsajid4@gmail.com>
2025-07-04 18:00:22 +0500

    Work on shop

--- schema/shop-schema.js
+++ schema/shop-schema.js
@@ -3,44 +3,49 @@ const Schema = mongoose.Schema;
 
 // Helper function to generate a random 6-digit number
 function generateSixDigitCode() {
-    return Math.floor(100000 + Math.random() * 900000); // Ensures 6-digit number
+  return Math.floor(100000 + Math.random() * 900000); // Ensures 6-digit number
 }
 
 const ShopSchema = new Schema({
-    itemPic: {
-        type: String,
-        required: true,
-    },
-    itemPrice: {
-        type: String,
-        required: true,
-    },
-    itemName: {
-        type: String,
-        required: true,
-    },
-    itemCode: {
-        type: Number,
-        unique: true, // Ensure itemCode is unique
-    },
-    category: { type: String, required: true },
+  itemPic: {
+    type: String,
+    required: true,
+  },
+  itemPrice: {
+    type: String,
+    required: true,
+  },
+  itemName: {
+    type: String,
+    required: true,
+  },
+  itemCode: {
+    type: Number,
+    unique: true, // Ensure itemCode is unique
+  },
+  category: { type: String, required: true },
+  prices: {
+    "7day": { type: Number, default: 0 },
+    "14day": { type: Number, default: 0 },
+    "30day": { type: Number, default: 0 },
+  },
 });
 
 // Pre-save middleware to auto-generate itemCode
 ShopSchema.pre("save", async function (next) {
-    if (!this.itemCode) {
-        let unique = false;
-        let code;
+  if (!this.itemCode) {
+    let unique = false;
+    let code;
 
-        while (!unique) {
-            code = generateSixDigitCode();
-            const existing = await mongoose.models.Shop.findOne({ itemCode: code });
-            if (!existing) unique = true;
-        }
-
-        this.itemCode = code;
+    while (!unique) {
+      code = generateSixDigitCode();
+      const existing = await mongoose.models.Shop.findOne({ itemCode: code });
+      if (!existing) unique = true;
     }
-    next();
+
+    this.itemCode = code;
+  }
+  next();
 });
 
 module.exports = mongoose.model("Shop", ShopSchema);
