adc22989817d6cdeda42bca1c1fb0523c75b3e6f
Abubakkar Sajid <abubakkarsajid4@gmail.com>
2025-07-13 11:12:49 +0500

    Work on chat

--- schema/Conversation.js
+++ schema/Conversation.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 const conversationSchema = new mongoose.Schema({
   members: [
     {
-      type:number,
-      required: true,
-      ref: "AccountCreate", // Assuming you have an AccountCreate model
-      
+      type:Number,
+      required: true,     
     }
   ],
 }, { timestamps: true });
