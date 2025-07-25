c1569b8ee877f646ee858ca4919f16e94464eda5
Abubakkar Sajid <abubakkarsajid4@gmail.com>
2025-07-25 14:53:42 +0500

    Wor k on server

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const NodeCache = require("node-cache");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', true); // ✅ Add this
 
 // In-memory cache
 const cache = new NodeCache();
