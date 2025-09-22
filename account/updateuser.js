ddfeb4af9f1152b86c8258d2db75e78a653cafb5
abubakkar <abubakkarsajid4@gmail.com>
2025-09-22 17:14:26 +0500

    Work on updateDataForm

--- account/updateuser.js
+++ account/updateuser.js
@@ -2,7 +2,7 @@ const AccountCreate = require("../schema/account-create");
 
 const updateUser = async (req, res) => {
   try {
-    const { id, name, userName, avatarUrl } = req.body;
+    const { id, name, userName, avatarUrl , gold,ui_id,updateDataForm } = req.body;
 
     if (!id) {
       return res.status(400).json({ message: "User ID is required." });
@@ -13,16 +13,27 @@ const updateUser = async (req, res) => {
     if (name !== undefined) updateData.name = name;
     if (userName !== undefined) updateData.userName = userName;
     if (avatarUrl !== undefined) updateData.avatarUrl = avatarUrl;
+    if (gold !== undefined)updateData.gold = gold;
+    if (ui_id !== undefined)updateData.ui_id = ui_id;
 
     if (Object.keys(updateData).length === 0) {
       return res.status(400).json({ message: "No update data provided." });
     }
+      if(updateDataForm){
+        Object.keys(updateDataForm).forEach(key => {
+          if (updateDataForm[key] !== undefined) {
+            updateData[key] = updateDataForm[key];
+          }
+        });
+      }
 
+    // Find user by ID and update
     const updatedUser = await AccountCreate.findByIdAndUpdate(
       id,
       { $set: updateData },
       { new: true, runValidators: true } // ensure schema validation applies
     );
+    
 
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found." });
